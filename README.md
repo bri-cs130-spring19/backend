a78b3f468993053a66d9168b2f1eb13432ad9614
Jonathan Zaturensky <jonathan@airmap.com>
2019-05-09 18:01:10 -0700

    Add simple map example

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nodejs-example
+
+`cd mapexample`
+`npm install`
+`npm start`
+
+`localhost:3000/map`
---
+++ mapexample/routes/map.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+var AWS = require("aws-sdk");
+var zipcodes = require("zipcodes");
+
+AWS.config.update({
+  region: "us-west-2"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+/* GET map. */
+router.get('/', function(req, res, next) {
+	console.log("Scanning...");
+
+	var params = {
+	    TableName : "survey_responses"
+	};
+
+	docClient.scan(params, function(err, data) {
+	    if (err) {
+	        console.error("Unable to scan. Error:", JSON.stringify(err, null, 2));
+	    } else {
+	        console.log("Scan succeeded.");
+	        var responses = [];
+	        data.Items.forEach(function(response) {
+	            //console.log(response.postal_code);
+	            if (response.postal_code != undefined) {
+		            let zipres = zipcodes.lookup(response.postal_code);
+		            if (zipres != undefined) {
+		            	response.latitude = zipres.latitude;
+		            	response.longitude = zipres.longitude;
+		            	responses.push(response);
+		            }
+		        }
+	        });
+
+	       	res.render('map', { responses: responses });
+	    }
+	});
+
+});
+
+module.exports = router;
