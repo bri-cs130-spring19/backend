6fe746038a97a12cfd5f9c8da0fdcbe0151a3149
Jonathan Zaturensky <jonathan@airmap.com>
2019-05-16 12:42:10 -0700

    Add ethnicity endpoint

--- merchant-insights/routes/data.js
+++ merchant-insights/routes/data.js
@@ -25,7 +25,7 @@ router.get('/states', function(req, res, next) {
 	docClient.scan(params, function(err, data) {
 	    if (err) {
 	        console.error("Unable to scan. Error:", JSON.stringify(err, null, 2));
-	        res.send({"error": "Unable to scan. Error:"});
+	        res.status(400).send({"error": "Unable to scan. Error:"});
 	    } else {
 	        console.log("Scan succeeded.");
 	        console.log(data.Items.length)
@@ -89,7 +89,7 @@ router.get('/states/:id', function(req, res, next) {
 	docClient.scan(params, function(err, data) {
 	    if (err) {
 	        console.error("Unable to scan. Error:", JSON.stringify(err, null, 2));
-	        res.send({"error": "Unable to scan. Error:"});
+	        res.status(400).send({"error": "Unable to scan. Error:"});
 	    } else {
 	        console.log("Scan succeeded.");
 	        console.log(data.Items.length)
@@ -139,6 +139,42 @@ router.get('/states/:id', function(req, res, next) {
 	       		"percentFemale": (numberFemales/(numberMales+numberFemales)).toFixed(2),
 	       		"averageOverallSatisfaction": (totalSatisfaction/numberSatisfactions).toFixed(2),
 	       		"averageOverallSatisfactionByMonth": satisfactionsByMonth
+	       	});
+	    }
+	});
+
+});
+
+/* GET ethnicity. */
+router.get('/ethnicity', function(req, res, next) {
+	console.log("Scanning...");
+
+	var params = {
+	    TableName : "survey_responses"
+	};
+
+	docClient.scan(params, function(err, data) {
+	    if (err) {
+	        console.error("Unable to scan. Error:", JSON.stringify(err, null, 2));
+	        res.status(400).send({"error": "Unable to scan. Error:"});
+	    } else {
+	        console.log("Scan succeeded.");
+	        console.log(data.Items.length)
+
+	        var totalSatisfaction = 0;
+	        var numberSatisfactions = 0;
+	        var numberMales = 0;
+	        var numberFemales = 0;
+
+	        data.Items.forEach(function(response) {
+	        	if (response.overall_satisfaction != undefined && response.ethnicity == req.query.ethnicity) {
+	        		totalSatisfaction += response.overall_satisfaction;
+	        		numberSatisfactions++;
+	        	}
+	        });
+
+	       	res.send({
+	       		"averageOverallSatisfaction": (totalSatisfaction/numberSatisfactions).toFixed(2)
 	       	});
 	    }
 	});
